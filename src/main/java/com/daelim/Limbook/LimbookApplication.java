66dbe1fc0e6e3f1d4eedc735ac4f865bea7f85e7
yo82y <yo82yo36@gmail.com>
2022-04-12 22:28:13 +0900

    Initial commit

---
+++ src/main/java/com/daelim/Limbook/LimbookApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.Limbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimbookApplication.class, args);
+	}
+
+}
