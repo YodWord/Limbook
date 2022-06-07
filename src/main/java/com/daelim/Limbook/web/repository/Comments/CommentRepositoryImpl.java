b2564d6dc9deb0088026bac9d53d0c6eb0e72cd6
YodWord <yo82yo36@gmail.com>
2022-06-07 11:15:40 +0900

    프론트 변경사항 수정중 Comment

--- src/main/java/com/daelim/Limbook/web/repository/Comments/CommentRepositoryImpl.java
+++ src/main/java/com/daelim/Limbook/web/repository/Comments/CommentRepositoryImpl.java
@@ -31,13 +31,13 @@ public class CommentRepositoryImpl implements CommentRepository{
         Number key = null;
 
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
-        jdbcInsert.withTableName("board_comment").usingGeneratedKeyColumns("board_comment_number");
+        jdbcInsert.withTableName("board_comment").usingGeneratedKeyColumns("comment_id");
 
         Map<String, Object> params = new HashMap<>();
-        params.put("board_number", comment.getBoardNumber());
+        params.put("board_id", comment.getBoardNumber());
         params.put("user_id", user.getUser_id());
-        params.put("board_comment_contents", comment.getBoardCommentContents());
-        params.put("board_comment_create_date", comment.getBoardCreatedAt());
+        params.put("board_comment", comment.getBoardCommentContents());
+        params.put("comment_created_date", comment.getBoardCreatedAt());
 
         try{
             key = jdbcInsert.executeAndReturnKey(params);
@@ -50,7 +50,7 @@ public class CommentRepositoryImpl implements CommentRepository{
 
     @Override
     public Comment updateComment(Integer commentId, UpdateCommentDTO updateCommentDTO, User user) throws Exception {
-        String sql = "update board_comment set board_comment_contents = ? where board_comment_number = ? ";
+        String sql = "update board_comment set board_comment = ? where comment_id = ? ";
 
         try{
             jdbcTemplate.update(sql, updateCommentDTO.getComment(), commentId);
@@ -65,7 +65,7 @@ public class CommentRepositoryImpl implements CommentRepository{
     public Comment deleteComment(Integer commentId, User user) throws Exception {
         Comment comment = findById(commentId).get();
 
-        String sql = "delete from board_comment where board_comment_number = ?";
+        String sql = "delete from board_comment where comment_id = ?";
 
         try{
             jdbcTemplate.update(sql, commentId);
@@ -78,7 +78,7 @@ public class CommentRepositoryImpl implements CommentRepository{
 
     @Override
     public List<Comment> findByBoardId(Integer boardId) throws Exception {
-        String sql = "select * from board_comment where board_number = ?";
+        String sql = "select * from board_comment where board_id = ?";
 
         List<Comment> result = jdbcTemplate.query(sql, commentRowMapper(), boardId);
 
@@ -87,7 +87,7 @@ public class CommentRepositoryImpl implements CommentRepository{
 
     @Override
     public Optional<Comment> findById(Integer commentId) throws Exception {
-        String sql = "select * from board_comment where board_comment_number = ?";
+        String sql = "select * from board_comment where comment_id = ?";
 
         List<Comment> result = jdbcTemplate.query(sql, commentRowMapper(), commentId);
 
@@ -97,11 +97,11 @@ public class CommentRepositoryImpl implements CommentRepository{
     private RowMapper<Comment> commentRowMapper() {
         return (rs, rowNum) -> {
             Comment comment = new Comment();
-            comment.setCommentNumber(rs.getInt("board_comment_number"));
+            comment.setCommentNumber(rs.getInt("comment_id"));
             comment.setUserId(rs.getString("user_id"));
-            comment.setBoardNumber(rs.getInt("board_number"));
-            comment.setBoardCommentContents(rs.getString("board_comment_contents"));
-            comment.setBoardCreatedAt(rs.getTimestamp("board_comment_create_date"));
+            comment.setBoardNumber(rs.getInt("board_id"));
+            comment.setBoardCommentContents(rs.getString("board_comment"));
+            comment.setBoardCreatedAt(rs.getTimestamp("comment_created_date"));
 
             return comment;
         };
